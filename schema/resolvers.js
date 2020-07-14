59a9bb7a1d7e074213baf61da277a56ee5976d1a
SMPlay <pavlik.sokolov.2000@gmail.com>
2020-07-14 22:20:07 +0300

    fixed bug with cors

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,43 +1,47 @@
-const { compare, hash } = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const cookie = require('cookie');
-const SECRET_KEY = require('../secret')
-const User = require('../models/user');
+const { compare, hash } = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie");
+const SECRET_KEY = require("../secret");
+const User = require("../models/user");
 
 module.exports = {
   Query: {
-    getUser:  () => {
-      return User.findById('5f0d73eea63635e0bc6a6e58');
+    getUser: () => {
+      return User.findById("5f0d73eea63635e0bc6a6e58");
     },
-    login: async(_, { login, password }, { res }) => {
+    login: async (_, { login, password }, { res }) => {
       const user = await User.findOne({ login });
+
       if (!user) {
-        throw new Error('User does not exist!');
+        throw new Error("User does not exist!");
       }
       const isEqual = await compare(password, user.password);
       if (!isEqual) {
-        throw new Error('Password is incorrect!');
+        throw new Error("Password is incorrect!");
       }
-      const token = jwt.sign(
-        { id: user.id, login: user.login },
-        SECRET_KEY,
-        {
-          expiresIn: '1h'
-        }
-      );
-      res.cookie('token', jwt, {
-        httpOnly: true,
-        maxAge: 3600,
+      const token = jwt.sign({ id: user.id, login: user.login }, SECRET_KEY, {
+        expiresIn: "1h",
       });
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", token, {
+          httpOnly: true,
+          sameSite: "strict",
+          maxAge: 3600,
+          path: "/",
+        })
+      );
+
       return { id: user.id };
-    }
+    },
   },
   Mutation: {
-    registerUser: async ( _, { login, password }) => {
+    registerUser: async (_, { login, password }) => {
       try {
         const existingUser = await User.findOne({ login });
-        if ( existingUser ) {
-          throw new Error('User exists already.');
+
+        if (existingUser) {
+          throw new Error("User exists already.");
         }
         const hashedPassword = await hash(password, 12);
         const user = new User({
@@ -47,10 +51,10 @@ module.exports = {
 
         const result = await user.save();
 
-        return { id: result.id, login: user.login};
+        return { id: result.id, login: user.login };
       } catch (err) {
         throw err;
       }
-    }
-  }
-}
+    },
+  },
+};
--- server/app.js
+++ server/app.js
@@ -1,28 +1,43 @@
-const typeDefs  = require('../schema/typeDefs');
-const resolvers = require('../schema/resolvers');
-const getAuthenticatedUserId = require('../utils/getAuthenticatedUserId');
-const mongoose = require('mongoose');
-const { ApolloServer } = require('apollo-server');
+const typeDefs = require("../schema/typeDefs");
+const resolvers = require("../schema/resolvers");
+const getAuthenticatedUserId = require("../utils/getAuthenticatedUserId");
+const mongoose = require("mongoose");
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
+const cors = require("cors")
 
-const password = '43kusKdPlDqE22am';
-const dbName = 'cards';
-mongoose.connect(`mongodb+srv://ilya:${password}@cluster0.fyz4t.mongodb.net/${dbName}?retryWrites=true&w=majority`, { useNewUrlParser: true,useUnifiedTopology: true  });
+const password = "43kusKdPlDqE22am";
+const dbName = "cards";
+mongoose.connect(
+  `mongodb+srv://ilya:${password}@cluster0.fyz4t.mongodb.net/${dbName}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const app = express();
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req, res }) => {
-    const userId = getAuthenticatedUserId(req.headers.authorization || '');
+    const userId = getAuthenticatedUserId(req.headers.authorization || "");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
     return {
       userId,
-      res
-    }
-  }});
+      res,
+    };
+  },
+});
+
+server.applyMiddleware({
+  app
+});
 
 const dbConnection = mongoose.connection;
-dbConnection.on('error', err => console.log(`Connection error: ${err}`));
-dbConnection.once('open', () => console.log('Connected to DB!'));
+dbConnection.on("error", (err) => console.log(`Connection error: ${err}`));
+dbConnection.once("open", () => console.log("Connected to DB!"));
 
-server.listen(4001).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+app.listen({ port: 4001 }, () => {
+  console.log(`🚀  Server ready at`)
+})
